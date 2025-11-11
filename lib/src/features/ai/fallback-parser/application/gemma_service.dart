ae99eed42172844f53a88cc2297740aed3f31f31
Praveen-Pandiyan <pravinand.io@gmail.com>
2025-11-11 07:11:02 +0530

    feat: LLM parsing tool defenition

--- lib/src/features/ai/fallback-parser/application/gemma_service.dart
+++ lib/src/features/ai/fallback-parser/application/gemma_service.dart
@@ -1,7 +1,23 @@
-import 'package:flutter_gemma/core/api/flutter_gemma.dart';
-import 'package:flutter_gemma/core/model.dart';
+import 'package:flutter_gemma/flutter_gemma.dart';
 import 'package:namma_wallet/src/common/di/locator.dart';
 import 'package:namma_wallet/src/common/services/logger_interface.dart';
+import 'package:namma_wallet/src/features/home/domain/generic_details_model.dart'
+    show GenericDetailsModel;
+
+final List<Tool> _tools = [
+  const Tool(
+    name: 'is_exist_in_db',
+    description:
+        'Checks weather a particular ticket data is already exist in database',
+    parameters: {
+      'type': 'object',
+      'properties': {
+        'id': {'type': 'string', 'description': 'unique ticket id'},
+      },
+      'required': ['id'],
+    },
+  ),
+];
 
 /// Service class to interact with Gemma AI chat
 class GemmaChatService {
@@ -13,6 +29,7 @@ class GemmaChatService {
   Future<void> init() async {
     const token = String.fromEnvironment('HUGGINGFACE_TOKEN');
     if (token.isEmpty) {
+      _logger.warning('HUGGINGFACE_TOKEN not found in env');
       return;
     }
     await FlutterGemma.installModel(
@@ -31,5 +48,36 @@ class GemmaChatService {
           }
         })
         .install();
+  }
+
+  // single or batch process both handled here
+
+  Future<List<GenericDetailsModel>> _parseBatch(List<String> data) async {
+    // get the current model
+    final model = await FlutterGemma.getActiveModel(
+      maxTokens: 2048,
+      preferredBackend: PreferredBackend.gpu,
+    );
+
+    // Use model
+    final chat = await model.createChat(
+      supportsFunctionCalls: true,
+    );
+    List<GenericDetailsModel> _response = [];
+    await Future.forEach(data, (msg) async {
+      await chat.addQueryChunk(
+        Message.text(
+          text: '', //TODO: prompt
+        ),
+      );
+      await chat.addQueryChunk(
+        Message.text(text: msg, isUser: true),
+      );
+      final response = await chat.generateChatResponse();
+     // todo : handle function here
+      await chat.clearHistory();
+    });
+
+    await model.close();
   }
 }
