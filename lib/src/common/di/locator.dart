0ab79ec42c91d7693152c27466becc095a73c34f
AkashProcoder <akashprocoder@gmail.com>
2025-12-01 11:52:57 +0530

    feat: migrate widget service from android widget branch

--- lib/src/common/di/locator.dart
+++ lib/src/common/di/locator.dart
@@ -13,6 +13,8 @@ import 'package:namma_wallet/src/common/services/ocr/google_mlkit_ocr.dart';
 import 'package:namma_wallet/src/common/services/ocr/ocr_service_interface.dart';
 import 'package:namma_wallet/src/common/services/pdf/pdf_service.dart';
 import 'package:namma_wallet/src/common/services/pdf/pdf_service_interface.dart';
+import 'package:namma_wallet/src/common/services/widget/home_widget_service.dart';
+import 'package:namma_wallet/src/common/services/widget/widget_service_interface.dart';
 import 'package:namma_wallet/src/common/theme/theme_provider.dart';
 import 'package:namma_wallet/src/features/ai/fallback_parser/application/ai_service_interface.dart';
 import 'package:namma_wallet/src/features/ai/fallback_parser/application/gemma_service.dart';
@@ -61,6 +63,12 @@ void setupLocator() {
     )
     ..registerLazySingleton<IAIService>(
       () => GemmaService(logger: getIt<ILogger>()),
+    )
+    ..registerLazySingleton<IWidgetService>(
+      () => HomeWidgetService(
+        logger: getIt<ILogger>(),
+        ticketDAO: getIt<ITicketDAO>(),
+      ),
     )
     // Parsers
     ..registerLazySingleton<TNSTCSMSParser>(TNSTCSMSParser.new)
