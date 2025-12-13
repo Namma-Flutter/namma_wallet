4bf48fdf0be28dd986c7a1bd6799a5e4c1a5dc6a
Praveen-Pandiyan <pravinand.io@gmail.com>
2025-12-13 11:36:20 +0530

    feat:sms reading impl

--- lib/src/features/sms_extract/application/sms_service.dart
+++ lib/src/features/sms_extract/application/sms_service.dart
@@ -1,37 +1,38 @@
-
 import 'package:android_sms_reader/android_sms_reader.dart';
+import 'package:flutter/foundation.dart';
 
- Future<void> _initPlugin() async {
-    final granted = await AndroidSMSReader.requestPermissions();
-    if (granted) {
-      await _loadInitialMessages();
-      _startSmsStream();
-    } else {
-      if (kDebugMode) {
-        print("SMS permission denied");
-      }
-    }
-  }
 
-  Future<void> _loadInitialMessages() async {
+Future<List<AndroidSMSMessage?>> initSMSRead() async {
+  print("inside fun");
+  bool isListening = false;
+  List<AndroidSMSMessage> messages = [];
+  Stream<AndroidSMSMessage>? smsStream;
+  final granted = await AndroidSMSReader.requestPermissions();
+  if (granted) {
+    print("granted");
     final fetched = await AndroidSMSReader.fetchMessages(
       type: AndroidSMSType.inbox,
       start: 0,
-      count: 20, 
-      
+      count: 20,
     );
-    setState(() {
-      messages = fetched;
-    });
+    print(fetched.length);
+    for (final element in fetched) {
+      print(element.address);
+      print(element.date);
+      print(element.type);
+      print(element.body);
+    }
+    return fetched;
+    // if (isListening) return;
+    // smsStream = AndroidSMSReader.observeIncomingMessages()
+    // ..listen((AndroidSMSMessage message) {
+    //   messages.insert(0, message); // insert latest at top
+    // });
+    // isListening = true;
+  } else {
+    if (kDebugMode) {
+      print("SMS permission denied");
+    }
+    return [];
   }
-
-  void _startSmsStream() {
-    if (isListening) return;
-    smsStream = AndroidSMSReader.observeIncomingMessages();
-    smsStream!.listen((AndroidSMSMessage message) {
-      setState(() {
-        messages.insert(0, message); // insert latest at top
-      });
-    });
-    isListening = true;
-  }
+}
