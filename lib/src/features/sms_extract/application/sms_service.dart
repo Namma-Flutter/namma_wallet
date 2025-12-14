eb8efdb6182bfcdb066cdd6dfc6865429cb0bd13
Praveen-Pandiyan <pravinand.io@gmail.com>
2025-12-14 12:54:44 +0530

    chore: sms services init

--- lib/src/features/sms_extract/application/sms_service.dart
+++ lib/src/features/sms_extract/application/sms_service.dart
@@ -1,15 +1,14 @@
 import 'package:android_sms_reader/android_sms_reader.dart';
 import 'package:flutter/foundation.dart';
+import 'package:get_it/get_it.dart';
+import 'package:namma_wallet/src/common/services/logger/logger_interface.dart';
+import 'package:namma_wallet/src/features/ai/fallback_parser/application/gemma_service.dart';
 
 
-Future<List<AndroidSMSMessage?>> initSMSRead() async {
-  print("inside fun");
-  bool isListening = false;
-  List<AndroidSMSMessage> messages = [];
-  Stream<AndroidSMSMessage>? smsStream;
+Future<List<AndroidSMSMessage?>> initSMSParse() async {
+  final getIt = GetIt.instance;
   final granted = await AndroidSMSReader.requestPermissions();
   if (granted) {
-    print("granted");
     final fetched = await AndroidSMSReader.fetchMessages(
       type: AndroidSMSType.inbox,
       start: 0,
@@ -22,13 +21,10 @@ Future<List<AndroidSMSMessage?>> initSMSRead() async {
       print(element.type);
       print(element.body);
     }
+    final gemmaService= GemmaService(logger: getIt<ILogger>());
+   await gemmaService.init();
+   await gemmaService.parseBatch(fetched.map((e)=>e.body).toList());
     return fetched;
-    // if (isListening) return;
-    // smsStream = AndroidSMSReader.observeIncomingMessages()
-    // ..listen((AndroidSMSMessage message) {
-    //   messages.insert(0, message); // insert latest at top
-    // });
-    // isListening = true;
   } else {
     if (kDebugMode) {
       print("SMS permission denied");
