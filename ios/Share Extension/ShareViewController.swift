193bb4d44c8fcaf0006f98e04520c9941feb742c
harishwarrior <warriorharish95668@gmail.com>
2026-01-13 17:09:24 +0530

    feat(ios): add share extension for receiving PDF intents
    Add iOS Share Extension to handle PDF sharing intents, including necessary entitlements, storyboard, and view controller. This enables the app to receive and process shared PDF files from other applications.

---
+++ ios/Share Extension/ShareViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ShareViewController.swift
+//  Share Extension
+//
+//  Created by Harish on 13/01/26.
+//
+
+// If you get no such module 'listen_sharing_intent' error.
+// Go to Build Phases of your Runner target and
+// move `Embed Foundation Extension` to the top of `Thin Binary`.
+import listen_sharing_intent
+
+class ShareViewController: RSIShareViewController {
+    /// Use this method to return false if you don't want to redirect to host app automatically.
+    /// Default is true
+    override func shouldAutoRedirect() -> Bool {
+        return false
+    }
+}
