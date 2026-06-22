78ce8989648c71ba5389a94398a464b3208f0581
harishwarrior <warriorharish95668@gmail.com>
2026-06-22 20:03:39 +0530

    fix: add missing semicolon in AppRoute enum definition

--- lib/src/common/routing/app_routes.dart
+++ lib/src/common/routing/app_routes.dart
@@ -25,7 +25,8 @@ enum AppRoute {
 
   // Debug routes
   dbViewer(path: '/db-viewer', name: 'dbViewer'),
-  ocrDebug(path: '/ocr-debug', name: 'ocrDebug');
+  ocrDebug(path: '/ocr-debug', name: 'ocrDebug')
+  ;
 
   const AppRoute({required this.path, required this.name});
 
